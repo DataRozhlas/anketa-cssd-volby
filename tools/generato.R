6c9120f51740da1c6551f49c46973195ec3e49ce
Petr Kočí <petr.koci@samizdat.cz>
2020-10-06 15:38:53 +0200

    první nához

--- tools/generato.R
+++ tools/generato.R
@@ -2,9 +2,18 @@ library(jsonlite)
 library(readr)
 library(magick)
 library(dplyr)
+library(stringr)
 
 data <- read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vSdS2s4DV_wy9zcK5pA8Oz83NzwT5Xdhw_UxceP2quT-tIkXbK5fOn4g030lNNDonjhDW0E3pigElEn/pub?gid=0&single=true&output=csv")
 
+# stáhni fotky
+
+for(i in data$f) {
+  obr <- image_read(i)
+  filename <- sub(pattern = "(.*)\\..*$", replacement = "\\1", basename(i))
+  image_write(obr, paste0("../foto/orig/", filename, ".jpg"), format = "jpeg")
+}
+  
 # ořízni fotky
 
 for(i in data$f) {
@@ -17,7 +26,7 @@ for(i in data$f) {
 
 # udělej JSON
 
-data <- data %>% select(1:7)
-data$f <- basename(data$f)
+data <- data %>% select(1:6)
+data$f <- paste0("../foto/orez/", sub(pattern = "(.*)\\..*$", replacement = "\\1", basename(data$f)), ".jpg")
 
 write_json(data, path="../data/data.json", na="null")
